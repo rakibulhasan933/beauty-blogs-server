e033ddc55ac26ec0f89dc023b0b32f5386c62c84
RAKIBUL <rakibulmd933@gmail.com>
2022-07-20 14:54:29 +0600

    database conected

--- index.js
+++ index.js
@@ -9,8 +9,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.41bbapx.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("beautyBlogs");
+        const blogsCollation = database.collection('blogs');
+        console.log("Database Connected");
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('CRUD Server Running in  Web')
 })
 
 app.listen(port, () => {
