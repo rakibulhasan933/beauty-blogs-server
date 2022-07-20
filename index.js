c79b4a7d787e34617a0c39cbc59c415c2f847b37
RAKIBUL <rakibulmd933@gmail.com>
2022-07-20 21:06:33 +0600

    file data saved

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
+const fileUpload = require('express-fileupload');
 
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.41bbapx.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -17,8 +19,27 @@ async function run() {
     try {
         await client.connect();
         const database = client.db("beautyBlogs");
-        const blogsCollation = database.collection('blogs');
         console.log("Database Connected");
+        const blogsCollation = database.collection('blogs');
+
+        app.post('/createBlogs', async (req, res) => {
+            const name = req.body.name;
+            const tittle = req.body.tittle;
+            const dec = req.body.dec;
+            const pic = req.files.image;
+            const picData = pic.data;
+            const encodedPic = picData.toString('base64');
+            const imageBuffer = Buffer.from(encodedPic, 'base64');
+            const blogsCreate = {
+                name,
+                tittle,
+                dec,
+                image: imageBuffer
+            }
+            const result = await blogsCollation.insertOne(blogsCreate);
+            res.json(result);
+        })
+
 
     } finally {
         // await client.close();
