cf0015043135a6fcf394ba5d9840082a83da1f5d
RAKIBUL <rakibulmd933@gmail.com>
2022-07-20 14:26:44 +0600

    cors import

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express')
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server Running on port ${port}`)
 });
